6eb54dbaa3f40ca67734b2a8b06eb573f1e26524
XiaoMing <1130465263@qq.com>
2021-08-07 10:15:25 +0800

    更新一下

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,42 @@
 <template>
-<div>
+<div id="home">
   <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   <home-swiper :banners=banners></home-swiper>
   <home-recommend-view :recommends=recommends></home-recommend-view>
+  <feature-view></feature-view>
+
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
 </div>
 </template>
 
@@ -10,6 +44,7 @@
 import NavBar from 'components/common/navbar/NavBar.vue'
 import HomeSwiper from './childComps/HomeSwiper.vue'
 import HomeRecommendView from './childComps/HomeRecommendView.vue'
+import FeatureView from './childComps/FeatureView.vue'
 import {getHomeMultidata} from 'network/home.js'
 
 export default {
@@ -17,7 +52,8 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    HomeRecommendView
+    HomeRecommendView,
+    FeatureView
   },
   data(){
     return{
@@ -36,8 +72,17 @@ export default {
 </script>
 
 <style>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color:#fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
 </style>
--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -1,13 +1,21 @@
 <template>
-  
+<div class="feature">
+  <a href="">
+    <img src="~assets/img/home/recommend_bg.jpg" alt="">
+  </a>
+</div>
 </template>
 
 <script>
 export default {
+  name: "FeatureView"
 
 }
 </script>
 
 <style>
+.feature img {
+  width: 100%;
+}
 
 </style>
